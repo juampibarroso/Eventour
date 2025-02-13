08085d8892e7592bc6ab2badb9c49747b1c8e843
Juan Pablo Barroso <juampibarroso164@gmail.com>
2025-02-13 19:29:06 -0300

    Se agregan los cuatro cuadros ne la pagina principal

---
+++ eventour-frontend/src/pages/EventosDestacados.jsx
@@ -0,0 +1,11 @@
+const EventosDestacados = () => {
+    return (
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Eventos Destacados</h1>
+        <p>Aquí podrás ver los eventos más destacados.</p>
+      </div>
+    );
+  };
+  
+  export default EventosDestacados;
+  
