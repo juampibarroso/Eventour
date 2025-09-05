23cac890b55a497f8480fe772d3e3f8ac61c10f1
Joaquin Esposito <joaquiesposito@gmail.com>
2025-09-05 18:04:49 -0300

    eliminando otra referencia en localidad para deployar

--- eventour-backend/src/main/java/com/eventour/eventour/repository/UbicacionRepository.java
+++ eventour-backend/src/main/java/com/eventour/eventour/repository/UbicacionRepository.java
@@ -7,10 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UbicacionRepository extends JpaRepository<Ubicacion, Long> {
-    List<Ubicacion> findByOasisAndLocalidad(Oasis oasis, String localidad);
 
     List<Ubicacion> findByOasis(Oasis oasis);
 
-    List<Ubicacion> findByLocalidad(String localidad);
+
 
 }
